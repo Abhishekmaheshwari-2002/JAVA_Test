e4d7962d8ff8530663d29268412fb5bba0544b90
Abhishek Maheshwari <71098003+Abhishekmaheshwari-2002@users.noreply.github.com>
2024-02-26 09:08:58 +0530

    Update prime.java

--- src/main/java/com/example/java_test/prime.java
+++ src/main/java/com/example/java_test/prime.java
@@ -6,7 +6,7 @@ public class prime {
             System.out.println("It is not Prime number");
         } else {
             for (int i = 2; i < num / 2; i++) {
-                if (num % 2 == 0) {
+                if (num % i == 0) {
                     System.out.println("It is a Prime Number");
                 }
             }
